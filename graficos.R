53987b316071acc76786bd6cffe085e1a54e995b
Marco Mello <marmello@gmail.com>
2020-05-25 20:33:16 -0300

    Update graficos.R

--- graficos.R
+++ graficos.R
@@ -8,14 +8,14 @@
 ##### Post: Qual grafico devo fazer?
 ##### URL: https://marcoarmello.wordpress.com
 ##### https://marcomellolab.wordpress.com
-##### Autor: Marco Mello & Renata Muylaert
+##### Autor: Marco Mello
 ##### E-mail: marmello@gmail.com 
 ##### Titulo: Script para desenhar graficos no R
 ##### Publicado em 25 de maio de 2020 (versao em portgues).
 ##### Rodado no 4.0.0 (2020-04-24) -- "Arbor Day"
 ##### Fonte dos dados usados como exemplo: Gonçalves, Fernando, Ricardo S. Bovendorp, Gabrielle Beca, Carolina Bello, Raul Costa-Pereira, Renata L. Muylaert, Raisa R. Rodarte, et al. 2018. “ATLANTIC MAMMAL TRAITS: A Data Set of Morphological Traits of Mammals in the Atlantic Forest of South America.” Ecology 99 (2): 498–498. https://doi.org/10.1002/ecy.2106.
 
-##### Aviso: Você pode usar este script livremente para fins não comerciais por seu próprio risco. Não assumimos nenhuma responsabilidade pelo uso deste software, não transmitimos licença ou título sob nenhuma patente, direito autoral ou mascaramento de direito de trabalho sobre o produto. Reservamo-nos o direito de fazer alterações no software sem notificação. Também não declaramos ou garantimos que esse aplicativo seja adequado para o uso especificado sem testes ou modificações adicionais. Se esse script o ajudar a produzir algum trabalho acadêmico (artigo, livro, capítulo, dissertação etc.), por favor, reconheça os autores e cite a fonte.
+##### Aviso: Voce pode usar este script livremente para fins nao comerciais por seu proprio risco. Nao assumimos nenhuma responsabilidade pelo uso deste software, nao transmitimos licenca ou titulo sob nenhuma patente, direito autoral ou mascaramento de direito de trabalho sobre o produto. Reservamo-nos o direito de fazer alteracoes no software sem notificacao. Tambem nao declaramos ou garantimos que esse aplicativo seja adequado para o uso especificado sem testes ou modificacoes adicionais. Se esse script o ajudar a produzir algum trabalho academico (artigo, livro, capitulo, dissertacao etc.), por favor, reconheca os autores e cite a fonte.
 
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
